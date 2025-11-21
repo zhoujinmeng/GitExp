8668a9ec784d6c5ed0d584d9f41cd8112caf4944
Pan <1902516675@qq.com>
2025-11-21 17:55:42 +0800

    add main.cpp

--- LearnGit/main.cpp
+++ LearnGit/main.cpp
@@ -1,6 +1,8 @@
+#include <iostream>
 #include "add.h"
 int main() {
     int x = 3, y = 4;
     std::cout << x << " + " << y << " = " << add(x, y) << std::endl;
+    std::cout << a << "-" << b << "=" << sub(a, b) << std::endl;  // ĞÂÔö
     return 0;
 }
