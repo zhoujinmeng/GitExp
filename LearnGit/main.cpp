90ad8c499995aeb6f70152201c00f77fd94c789b
zhou <1960548815@qq.com>
2025-11-21 23:54:28 +0800

    zhou

--- LearnGit/main.cpp
+++ LearnGit/main.cpp
@@ -4,6 +4,6 @@ int main() {
     int x = 3, y = 4;
     std::cout << x << " + " << y << " = " << add(x, y) << std::endl;
     std::cout << y << "-" << x << "=" << sub(y,x) << std::endl;  // ����
-    cout << "This is a change from zhou." << endl;
+    std::cout << "This is a change from zhou." << std::endl;
     return 0;
 }
