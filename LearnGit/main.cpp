0de3eb5a8410326e20d244806c2ad65e19f6c167
zhou <1960548815@qq.com>
2025-11-21 23:52:36 +0800

    zhou

--- LearnGit/main.cpp
+++ LearnGit/main.cpp
@@ -3,6 +3,7 @@
 int main() {
     int x = 3, y = 4;
     std::cout << x << " + " << y << " = " << add(x, y) << std::endl;
-    std::cout << y << "-" << x << "=" << sub(y,x) << std::endl;  // ĞÂÔö
+    std::cout << y << "-" << x << "=" << sub(y,x) << std::endl;  // ï¿½ï¿½ï¿½ï¿½
+    cout << "This is a change from zhou." << endl;
     return 0;
 }
--- helloworld.cpp
+++ helloworld.cpp
@@ -3,5 +3,5 @@ using namespace std;
 
 int main() {
 	cout << "hello world!" << endl;
-	cout << "This is a change from zhou." << endl;
+	
 }
